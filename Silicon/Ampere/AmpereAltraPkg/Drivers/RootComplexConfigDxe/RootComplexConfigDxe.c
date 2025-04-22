469457b5a0e6c489bb859e7a0ce8ce72668cc067
manimehalai.s <manimehalai.s@adlinktech.com>
2025-04-22 15:45:01 +0530

    Reverting the previous commit, as x16 Bifurcation is not supported on Root Complexes 4 and 7 as per hardware schematics

--- Silicon/Ampere/AmpereAltraPkg/Drivers/RootComplexConfigDxe/RootComplexConfigDxe.c
+++ Silicon/Ampere/AmpereAltraPkg/Drivers/RootComplexConfigDxe/RootComplexConfigDxe.c
@@ -782,11 +782,12 @@ PcieRCScreenSetup (
   VOID               *EndOpCodeHandle;
   VOID               *OptionsOpCodeHandle;
   VOID               *StartOpCodeHandle;
+  UINT16             MaxCores; 
 //><ADLINK-MS20232710>//
   EFI_STRING_ID      StrId;
 //><ADLINK-MS20232710>//
   RootComplex = GetRootComplex (RCIndex);
-
+  MaxCores = GetMaximumNumberOfCores();
   // Initialize the container for dynamic opcodes
   StartOpCodeHandle = HiiAllocateOpCodeHandle ();
   ASSERT (StartOpCodeHandle != NULL);
@@ -869,7 +870,7 @@ PcieRCScreenSetup (
       NULL
       ),                                       // Prompt
     STRING_TOKEN (STR_PCIE_RC_STATUS_HELP),    // Help
-    (RCIndex == 6) ? QuestionFlags | EFI_IFR_LOCKED_OP : QuestionFlags,     // Question flag
+    (RCIndex == 6) ? QuestionFlags | EFI_IFR_FLAG_READ_ONLY : QuestionFlags,     // Question flag
     0,                                         // CheckBoxFlags
     NULL                                       // DefaultsOpCodeHandle
     );
@@ -880,12 +881,6 @@ PcieRCScreenSetup (
     //
     OptionsOpCodeHandle = CreateDevMapOptions (RootComplex);
 
-    QuestionFlagsSubItem = QuestionFlags;
-    if (!(IsAc01Processor())) {
-      if((RCIndex == 4) || (RCIndex == 6) || (RCIndex == 7)) {
-        QuestionFlagsSubItem |= EFI_IFR_LOCKED_OP;
-      }
-    }
 
     HiiCreateOneOfOpCode (
       StartOpCodeHandle,                        // Container for dynamic created opcodes
@@ -894,22 +889,18 @@ PcieRCScreenSetup (
       BifurLoVarOffset,                         // Offset in Buffer Storage
       STRING_TOKEN (STR_PCIE_RCA_BIFUR),        // Question prompt text
       STRING_TOKEN (STR_PCIE_RCA_BIFUR_HELP),   // Question help text
-      QuestionFlagsSubItem,                            // Question flag
+	 (RCIndex == 6 && MaxCores == 128) ? QuestionFlags | EFI_IFR_FLAG_READ_ONLY : QuestionFlags,     // Question flag
       EFI_IFR_NUMERIC_SIZE_1,                   // Data type of Question Value
       OptionsOpCodeHandle,                      // Option Opcode list
       NULL                                      // Default Opcode is NULl
       );
-      QuestionFlagsSubItem = 0;
   } else {
     //
     // Create Option OpCode to display bifurcation for RootComplexTypeB-Low
     //
     OptionsOpCodeHandle = CreateDevMapOptions (RootComplex);
+
     QuestionFlagsSubItem = QuestionFlags;
-    
-    if(RCIndex == 6) {
-      QuestionFlagsSubItem |= EFI_IFR_LOCKED_OP;
-    }
     HiiCreateOneOfOpCode (
       StartOpCodeHandle,                         // Container for dynamic created opcodes
       0x8003 + MAX_EDITABLE_ELEMENTS * RCIndex,  // Question ID (or call it "key")
@@ -917,7 +908,8 @@ PcieRCScreenSetup (
       BifurLoVarOffset,                          // Offset in Buffer Storage
       STRING_TOKEN (STR_PCIE_RCB_LO_BIFUR),      // Question prompt text
       STRING_TOKEN (STR_PCIE_RCB_LO_BIFUR_HELP), // Question help text
-      QuestionFlagsSubItem,                      // Question flag
+	  (RCIndex == 6) ? QuestionFlags | EFI_IFR_FLAG_READ_ONLY :  QuestionFlagsSubItem,   // Question flag
+      //QuestionFlagsSubItem,                      // Question flag
       EFI_IFR_NUMERIC_SIZE_1,                    // Data type of Question Value
       OptionsOpCodeHandle,                       // Option Opcode list
       NULL                                       // Default Opcode is NULl
@@ -929,10 +921,6 @@ PcieRCScreenSetup (
     OptionsOpCodeHandle = CreateDevMapOptions (RootComplex);
 
     QuestionFlagsSubItem = QuestionFlags;
-   
-    if(RCIndex == 6) {
-      QuestionFlagsSubItem |= EFI_IFR_LOCKED_OP;
-    }
     HiiCreateOneOfOpCode (
       StartOpCodeHandle,                         // Container for dynamic created opcodes
       0x8004 + MAX_EDITABLE_ELEMENTS * RCIndex,  // Question ID (or call it "key")
@@ -940,7 +928,8 @@ PcieRCScreenSetup (
       BifurHiVarOffset,                          // Offset in Buffer Storage
       STRING_TOKEN (STR_PCIE_RCB_HI_BIFUR),      // Question prompt text
       STRING_TOKEN (STR_PCIE_RCB_HI_BIFUR_HELP), // Question help text
-      QuestionFlagsSubItem,                      // Question flag
+	  (RCIndex == 6) ? QuestionFlags | EFI_IFR_FLAG_READ_ONLY :  QuestionFlagsSubItem,    // Question flag
+      //QuestionFlagsSubItem,                      // Question flag
       EFI_IFR_NUMERIC_SIZE_1,                    // Data type of Question Value
       OptionsOpCodeHandle,                     // Option Opcode list
       NULL                                       // Default Opcode is NULl
@@ -950,12 +939,12 @@ PcieRCScreenSetup (
 //><ADLINK-MS20232710>//
   for (UINT8 PcieIndex=0; PcieIndex < AC01_PCIE_MAX_RCS_PER_SOCKET; PcieIndex++)
   {
-    QuestionFlagsSubItem = QuestionFlags;
-    // Check Read-Only Conditions
-    if (((IsAc01Processor()) && RCIndex == 6) ||
-      ((!IsAc01Processor()) && ((RCIndex == 6 && (PcieIndex == 0 || PcieIndex == 1)) || 
-      (RCIndex == 7 && PcieIndex == 0)))) {
-      QuestionFlagsSubItem |= EFI_IFR_LOCKED_OP;
+  	  QuestionFlagsSubItem = QuestionFlags;
+	  // Check Read-Only Conditions
+	  if ((MaxCores < 128 && RCIndex == 6) ||
+        (MaxCores == 128 && ((RCIndex == 6 && (PcieIndex == 0 || PcieIndex == 1)) || 
+                             (RCIndex == 7 && PcieIndex == 0)))) {
+      QuestionFlagsSubItem |= EFI_IFR_FLAG_READ_ONLY;
       }
   	  
   	  if (RootComplex->Pcie[PcieIndex].Active == 0)
